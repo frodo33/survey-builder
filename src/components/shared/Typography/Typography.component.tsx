06a01c99e767d88a798bfdb54538aed75fcabe47
frodo33 <to.gwozdz@gmail.com>
2026-04-07 20:50:24 +0200

    feat: update typography styles

--- src/components/shared/Typography/Typography.component.tsx
+++ src/components/shared/Typography/Typography.component.tsx
@@ -1,9 +1,10 @@
 import type { ElementType } from "react"
 
+import { cn } from "@/lib/utils"
+
 import { typographyVariants } from "./Typography.styles"
 import type { TypographyProps } from "./Typography.types"
 import { variantToTag } from "./Typography.utils"
-import { cn } from "@/lib/utils"
 
 export const Typography = <E extends ElementType = "p">({
   as,
--- src/components/shared/Typography/Typography.styles.ts
+++ src/components/shared/Typography/Typography.styles.ts
@@ -6,7 +6,7 @@ export const typographyVariants = cva("", {
       h1: "text-3xl leading-tight font-bold tracking-tight",
       h2: "text-2xl leading-snug font-semibold tracking-tight",
       h3: "text-xl leading-snug font-semibold tracking-tight",
-      p: "text-base leading-7",
+      p: "text-sm leading-6",
       small: "text-xs leading-none font-medium",
     },
   },
