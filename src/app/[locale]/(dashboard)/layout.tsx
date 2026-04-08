9fc9f8c9dae0c84bf8ff562e7a7e29fd0fd812d6
frodo33 <to.gwozdz@gmail.com>
2026-04-08 21:47:18 +0200

    style: increase content padding on desktop

--- src/app/[locale]/(dashboard)/layout.tsx
+++ src/app/[locale]/(dashboard)/layout.tsx
@@ -15,7 +15,7 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
       <SidebarInset className="mx-auto max-w-7xl">
         <div className="bg-sidebar absolute flex h-full w-full flex-1 flex-col overflow-y-hidden">
           <AppHeader />
-          <main className="bg-background scrollbar-subtle mx-4 mb-4 flex flex-1 flex-col overflow-scroll rounded-lg p-4">
+          <main className="bg-background scrollbar-subtle mx-4 mb-4 flex flex-1 flex-col overflow-scroll rounded-lg p-4 lg:p-8">
             {children}
           </main>  
         </div>
