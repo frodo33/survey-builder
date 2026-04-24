441eda2630f7b4edd33e4ef014aa94e532331883
frodo33 <to.gwozdz@gmail.com>
2026-04-24 18:10:17 +0200

    tests: improve useTranslations mock

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,5 +1,10 @@
 import "@testing-library/jest-dom/vitest"
 
 vi.mock("next-intl", () => ({
-  useTranslations: () => (key: string) => key
-}))
+  useTranslations: () =>
+    (key: string, params?: Record<string, unknown>) => {
+      if (!params) return key;
+
+      return `${key} ${JSON.stringify(params)}`;
+    },
+}));
